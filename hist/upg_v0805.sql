bd3adad9af20419db4043ac9848835d60eb4a70e
Jeremy D Monin <jdmonin@nand.net>
2010-12-01 05:38:23 +0000

    Doc files were in wrong locaton, deleted [svn r4]

--- hist/upg_v0805.sql
+++
@@ -1,7 +0,0 @@
--- upgrade from v0800 to v0805: (2010-09-06)
-BEGIN TRANSACTION;
-ALTER TABLE vehicle ADD COLUMN odo_orig integer not null DEFAULT 0;
-ALTER TABLE vehicle ADD COLUMN odo_curr integer not null DEFAULT 0;
-ALTER TABLE vehicle ADD COLUMN last_tripid integer;
-COMMIT;
-

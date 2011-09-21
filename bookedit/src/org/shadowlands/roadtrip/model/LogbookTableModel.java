6ad87f7631e09e88b54de73f2222e5e17d5421d1
Jeremy D Monin <jdmonin@nand.net>
2011-09-21 11:17:53 +0000

    bookedit: LogbookTableModel use RTRDateTimeFormatter [svn r153]

--- bookedit/src/org/shadowlands/roadtrip/model/LogbookTableModel.java
+++ bookedit/src/org/shadowlands/roadtrip/model/LogbookTableModel.java
@@ -34,6 +34,7 @@ import org.shadowlands.roadtrip.db.Trip;
 import org.shadowlands.roadtrip.db.Vehicle;
 import org.shadowlands.roadtrip.db.ViaRoute;
 import org.shadowlands.roadtrip.db.Trip.TripListTimeRange;
+import org.shadowlands.roadtrip.util.RTRDateTimeFormatter;
 
 /**
  * Renders and holds data for display or editing.
@@ -373,8 +374,7 @@ public class LogbookTableModel // extends javax.swing.table.AbstractTableModel
 			return;  // <--- nothing found ---
 		}
 
-		DateFormat dfd = DateFormat.getDateInstance(DateFormat.MEDIUM);
-		DateFormat dft = DateFormat.getTimeInstance(DateFormat.SHORT);
+		RTRDateTimeFormatter dtf = new RTRDateTimeFormatter();
 		final int tRowCount;  // rows in tText before add
 		if (ttr.tText == null)
 		{
@@ -404,7 +404,7 @@ public class LogbookTableModel // extends javax.swing.table.AbstractTableModel
 			    || (prevTripStart.getMonth() != tstart.getMonth()))
 			{
 				tr = new String[COL_HEADINGS.length];
-				tr[0] = dfd.format(tstart);
+				tr[0] = dtf.formatDate(tstart);
 				tText.addElement(tr);
 			}
 			prevTripStart = tstart;
@@ -425,7 +425,7 @@ public class LogbookTableModel // extends javax.swing.table.AbstractTableModel
 
 			// next row of trip: time
 			tr = new String[COL_HEADINGS.length];
-			tr[1] = dft.format(tstart);
+			tr[1] = dtf.formatTime(tstart);
 			tText.addElement(tr);
 
 			final int odo_end = t.getOdo_end();
@@ -452,7 +452,7 @@ public class LogbookTableModel // extends javax.swing.table.AbstractTableModel
 					if (ttstop != 0)
 					{
 						tr = new String[COL_HEADINGS.length];
-		    			tr[1] = dft.format(new Date(ttstop * 1000L));
+		    			tr[1] = dtf.formatTime(ttstop * 1000L);
 		    			tText.addElement(tr);
 					}
 	
@@ -550,7 +550,7 @@ public class LogbookTableModel // extends javax.swing.table.AbstractTableModel
 					if (ttcont != 0)
 					{
 						tr = new String[COL_HEADINGS.length];
-		    			tr[1] = dft.format(new Date(ttcont * 1000L));
+		    			tr[1] = dtf.formatTime(ttcont * 1000L);
 		    			tText.addElement(tr);
 					}
 				}

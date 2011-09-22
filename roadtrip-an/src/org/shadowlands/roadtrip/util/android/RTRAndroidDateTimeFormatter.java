7fb24b996564336bfe81896f0bd9d460c4643f14
Jeremy D Monin <jdmonin@nand.net>
2011-09-22 06:49:45 +0000

    RTRAndroidDateTimeFormatter.formatDate: Sat 9 Jun 2001, not just ##/##/#### [svn r161]

--- roadtrip-an/src/org/shadowlands/roadtrip/util/android/RTRAndroidDateTimeFormatter.java
+++ roadtrip-an/src/org/shadowlands/roadtrip/util/android/RTRAndroidDateTimeFormatter.java
@@ -19,21 +19,54 @@
 
 package org.shadowlands.roadtrip.util.android;
 
+import java.util.Date;
+
+import org.shadowlands.roadtrip.android.util.Misc;
 import org.shadowlands.roadtrip.util.RTRDateTimeFormatter;
 
 import android.content.Context;
+import android.text.format.DateFormat;
 
+/**
+ * Android-specific date/time formatting, based on user preference settings.
+ * @author jdmonin
+ */
 public class RTRAndroidDateTimeFormatter
 	extends RTRDateTimeFormatter
 {
 	/**
+	 * date formatter for use by {@link DateFormat#format(CharSequence, long)},
+	 * initialized via {@link Misc#buildDateFormatDOWShort(Context, boolean)}.
+	 */
+	private StringBuffer fmt_dow_meddate;
+
+	/**
 	 * Use the Android-specific date/time formats.
+	 * Date format will be: Sat 9 Jun 2001 (YMD order depends on user settings).
 	 * @param ctx App context for date/time preferences, from {@link android.app.Activity#getApplicationContext()}
 	 */
 	public RTRAndroidDateTimeFormatter(Context ctx)
 	{
-		dfd = android.text.format.DateFormat.getDateFormat(ctx);
+		// dfd is unused; was = android.text.format.DateFormat.getDateFormat(ctx),
+		//   but getDateFormat always gives ##/##/## or ##/##/####, never day of week.
+		fmt_dow_meddate = Misc.buildDateFormatDOWMed(ctx);
 		dft = android.text.format.DateFormat.getTimeFormat(ctx);
+	}
+
+	/**
+	 * Date format will be: Sat 9 Jun 2001 (YMD order depends on user settings).
+	 */
+	public String formatDate(final long millis)
+	{
+		return DateFormat.format(fmt_dow_meddate, millis).toString();
+	}
+
+	/**
+	 * Date format will be: Sat 9 Jun 2001 (YMD order depends on user settings).
+	 */
+	public String formatDate(final Date dt)
+	{
+		return DateFormat.format(fmt_dow_meddate, dt.getTime()).toString();
 	}
 
 }

7717ad6652c68a09075220bc00976bb40974940e
Jeremy D Monin <jdmonin@nand.net>
2012-03-31 15:09:01 +0000

    add setEnabled [svn r185]

--- roadtrip-an/src/org/shadowlands/roadtrip/android/OdometerNumberPicker.java
+++ roadtrip-an/src/org/shadowlands/roadtrip/android/OdometerNumberPicker.java
@@ -1,7 +1,7 @@
 /*
  *  This file is part of Shadowlands RoadTrip - A vehicle logbook for Android.
  *
- *  Copyright (C) 2010-2011 Jeremy D Monin <jdmonin@nand.net>
+ *  Copyright (C) 2010-2012 Jeremy D Monin <jdmonin@nand.net>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -304,6 +304,16 @@ public class OdometerNumberPicker extends LinearLayout implements OnChangedListe
 		View dot = (View) findViewById(R.id.org_shadowlands_rtr_odometer_dot);
 		if (dot != null)
 			dot.setVisibility(vis);
+	}
+
+	/**
+	 * Enable or disable our {@link NumberPicker}s.
+	 */
+	public void setEnabled(final boolean ena)
+	{
+		super.setEnabled(ena);
+		mWholeNum.setEnabled(ena);
+		mTenths.setEnabled(ena);
 	}
 
 	/**

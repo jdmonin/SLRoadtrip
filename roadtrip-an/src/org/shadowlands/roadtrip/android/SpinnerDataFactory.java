61dd6fb94f6a5a26d5b04bd7e3778237440e9985
Jeremy D Monin <jdmonin@nand.net>
2012-04-01 16:04:19 +0000

    (c)2012 [svn r208]

--- roadtrip-an/src/org/shadowlands/roadtrip/android/SpinnerDataFactory.java
+++ roadtrip-an/src/org/shadowlands/roadtrip/android/SpinnerDataFactory.java
@@ -1,7 +1,7 @@
 /*
  *  This file is part of Shadowlands RoadTrip - A vehicle logbook for Android.
  *
- *  Copyright (C) 2010-2011 Jeremy D Monin <jdmonin@nand.net>
+ *  Copyright (C) 2010-2012 Jeremy D Monin <jdmonin@nand.net>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by

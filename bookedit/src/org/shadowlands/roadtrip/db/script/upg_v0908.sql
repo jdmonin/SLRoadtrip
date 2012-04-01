faa1b209517a79bebc732b926f857d8b0e9cba98
Jeremy D Monin <jdmonin@nand.net>
2012-04-01 19:15:00 +0000

    schema: freqtrip + catid: bookedit [svn r222]

--- bookedit/src/org/shadowlands/roadtrip/db/script/upg_v0908.sql
+++ bookedit/src/org/shadowlands/roadtrip/db/script/upg_v0908.sql
@@ -1,4 +1,4 @@
--- Upgrade from v0906 to v0908: (2012-03-31)  [schema versions skip 0907]
+-- Upgrade from v0906 to v0908: (2012-04-01)  [schema versions skip 0907]
 
 -- This file is part of Shadowlands RoadTrip - A vehicle logbook for Android.
 -- 
@@ -26,8 +26,21 @@ ALTER TABLE freqtrip ADD COLUMN is_roundtrip int not null default 0;
 
 ALTER TABLE vehiclemake ADD COLUMN is_user_add int;
 
+CREATE TABLE tripcategory ( _id integer PRIMARY KEY AUTOINCREMENT not null, cname varchar(255) not null unique, pos int not null, is_user_add int );
+
+ALTER TABLE trip ADD COLUMN catid int;
+CREATE INDEX "trip~cv" ON trip(catid, vid);
+
+ALTER TABLE freqtrip ADD COLUMN catid int;
+
 CREATE TABLE app_db_upgrade_hist ( db_vers_to int not null, db_vers_from not null, upg_time int not null );
 
--- adds 2012-03-31 v0908: citroen = 52 renault = 53
+-- vehiclemake adds 2012-04-01 v0908: citroen = 52 renault = 53
 insert into vehiclemake(mname) values ('Citroen');
 insert into vehiclemake(mname) values ('Renault');
+
+-- tripcategory initial contents 2012-04-01 v0908:
+insert into tripcategory(cname,pos) values ('Work', 1);
+insert into tripcategory(cname,pos) values ('Personal', 2);
+insert into tripcategory(cname,pos) values ('Volunteer', 3);
+insert into tripcategory(cname,pos) values ('Moving', 4);

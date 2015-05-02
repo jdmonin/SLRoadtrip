dc51799fb00e5d17d07bfab06b3e03852d83d554
Jeremy D Monin <jdmonin@nand.net>
2015-05-02 23:01:59 +0000

    Released version 0.9.41 is r428; bump in-development version to v0.9.42 [svn r429]

--- bookedit/src/org/shadowlands/roadtrip/bookedit/Main.java
+++ bookedit/src/org/shadowlands/roadtrip/bookedit/Main.java
@@ -45,14 +45,14 @@ public class Main
 	 * @see #APP_VERSION_STRING
 	 * @since 0.9.07
 	 */
-	public static final int APP_VERSION_CODE = 941;
+	public static final int APP_VERSION_CODE = 942;
 
 	/**
 	 * Application version as a human-readable string; changes with each point release.
 	 * @see #APP_VERSION_CODE
 	 * @since 0.9.07
 	 */
-	public static final String APP_VERSION_STRING = "0.9.41";
+	public static final String APP_VERSION_STRING = "0.9.42";
 
 	private String dbFilename = null;
 	private StartupChoiceFrame scf;

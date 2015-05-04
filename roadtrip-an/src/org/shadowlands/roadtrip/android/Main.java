4af359c5274b28853d837d10ac9f3b486d159f3d
Jeremy D Monin <jdmonin@nand.net>
2015-05-03 20:28:16 -0500

    svn to git migration: roadtrip-an About box: rename svnversion.txt -> gitversion.txt

--- roadtrip-an/src/org/shadowlands/roadtrip/android/Main.java
+++ roadtrip-an/src/org/shadowlands/roadtrip/android/Main.java
@@ -1,7 +1,7 @@
 /*
  *  This file is part of Shadowlands RoadTrip - A vehicle logbook for Android.
  *
- *  This file Copyright (C) 2010-2012,2014 Jeremy D Monin <jdmonin@nand.net>
+ *  This file Copyright (C) 2010-2012,2014-2015 Jeremy D Monin <jdmonin@nand.net>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -210,7 +210,7 @@ public class Main extends Activity
 	/**
 	 * Create the About dialog.
 	 * The version number is determined from app resources.
-	 * The build number is read from res/raw/svnversion.txt which is manually
+	 * The build number is read from res/raw/gitversion.txt which is manually
 	 * updated by the developer before building an APK.
 	 */
 	@Override
@@ -258,10 +258,10 @@ public class Main extends Activity
 					}
 				} catch (NameNotFoundException e) { }
 
-				// Now try to get build number, from res/raw/svnversion.txt ; ignore "?"
+				// Now try to get build number, from res/raw/gitversion.txt ; ignore "?"
 				InputStream s = null;
 				try {
-					s = getApplicationContext().getResources().openRawResource(R.raw.svnversion);
+					s = getApplicationContext().getResources().openRawResource(R.raw.gitversion);
 					DataInputStream dsql = new DataInputStream(s);
 					String svnversion = dsql.readLine();
 					dsql.close();

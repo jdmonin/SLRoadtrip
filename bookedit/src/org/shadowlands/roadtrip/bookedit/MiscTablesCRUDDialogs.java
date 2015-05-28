cc9f73fcc27aa04b4746c1e850f9536e24ffb07e
Jeremy D Monin <jdmonin@nand.net>
2015-05-28 00:49:31 -0500

    bookedit createEditPersonDialog, createEditVehicleDialog: Show date_added

--- bookedit/src/org/shadowlands/roadtrip/bookedit/MiscTablesCRUDDialogs.java
+++ bookedit/src/org/shadowlands/roadtrip/bookedit/MiscTablesCRUDDialogs.java
@@ -38,6 +38,7 @@ public abstract class MiscTablesCRUDDialogs
 	{
 		MultiInputDialog.F_STRING | MultiInputDialog.F_FLAG_REQUIRED,
 		MultiInputDialog.F_BOOL,
+		MultiInputDialog.F_TIMESTAMP | MultiInputDialog.F_FLAG_READONLY,
 		MultiInputDialog.F_STRING
 	};
 
@@ -56,17 +57,19 @@ public abstract class MiscTablesCRUDDialogs
 	    (JFrame owner, RDBAdapter conn, final boolean isReadOnly, Person p, final boolean driverIfNew)
 	    throws IllegalStateException, NullPointerException
 	{
-		final String[] labels = { "Name", "Driver?", "Comment" };
-		String[] vals = new String[3];
+		final String[] labels = { "Name", "Driver?", "Date added", "Comment" };
+		String[] vals = new String[4];
 		if (p != null)
 		{
 			vals[0] = p.getName();
 			vals[1] = (p.isDriver() ? "Y" : "N");
-			vals[2] = p.getComment();
+			vals[2] = Integer.toString(p.getDateAdded());
+			vals[3] = p.getComment();
 		} else {
 			vals[0] = null;
 			vals[1] = (driverIfNew ? "Y" : "N");
 			vals[2] = null;
+			vals[3] = null;
 		}
 
 		/**
@@ -85,12 +88,13 @@ public abstract class MiscTablesCRUDDialogs
 		final boolean isDriver = (vals[1] != null) && (vals[1].equalsIgnoreCase("Y"));
 		if (p == null)
 		{
-			p = new Person(vals[0], isDriver, null, vals[2]);
+			p = new Person(vals[0], isDriver, null, vals[3]);
 			p.insert(conn);
 		} else if (mid.isChanged())
 		{
 			p.setName(vals[0]);
 			p.setIsDriver(isDriver);
+			p.setComment(vals[3]);
 			p.commit();
 		}
 
@@ -104,6 +108,7 @@ public abstract class MiscTablesCRUDDialogs
 		MultiInputDialog.F_DB_PERSON_DRIVER | MultiInputDialog.F_FLAG_REQUIRED,
 		MultiInputDialog.F_DB_VEHICLEMAKE | MultiInputDialog.F_FLAG_REQUIRED,
 		MultiInputDialog.F_STRING, MultiInputDialog.F_INT | MultiInputDialog.F_FLAG_REQUIRED,
+		MultiInputDialog.F_TIMESTAMP | MultiInputDialog.F_FLAG_READONLY,
 		MultiInputDialog.F_TIMESTAMP, MultiInputDialog.F_TIMESTAMP,
 		MultiInputDialog.F_STRING, MultiInputDialog.F_STRING,
 		MultiInputDialog.F_ODOMETER | MultiInputDialog.F_FLAG_READONLY,
@@ -128,9 +133,9 @@ public abstract class MiscTablesCRUDDialogs
 	{
 		// keep arrays congruent: labels[], FTYPES_DIA_VEHICLE[], vals[]
 		final String[] labels =
-			{ "Nickname", "Active?", "Main Driver", "Make", "Model", "Year", "Owned from", "Owned to",
-			  "VIN", "License plate/tag", "Original odometer", "Current odometer", "Comment" };
-		String[] vals = new String[13];
+			{ "Nickname", "Active?", "Main Driver", "Make", "Model", "Year", "Date added", "Owned from",
+			  "Owned to", "VIN", "License plate/tag", "Original odometer", "Current odometer", "Comment" };
+		String[] vals = new String[14];
 		if (v != null)
 		{
 			vals[0] = v.getNickname();
@@ -139,13 +144,16 @@ public abstract class MiscTablesCRUDDialogs
 			vals[3] = Integer.toString(v.getMakeID());
 			vals[4] = v.getModel();
 			vals[5] = Integer.toString(v.getYear());
-			vals[6] = Integer.toString(v.getDate_from());
-			vals[7] = Integer.toString(v.getDate_to());
-			vals[8] = v.getVin();
-			vals[9] = v.getPlate();
-			vals[10] = Integer.toString(v.getOdometerOriginal());
-			vals[11] = Integer.toString(v.getOdometerCurrent());
-			vals[12] = v.getComment();
+			final int da = v.getDate_added();
+			if (da != 0)
+				vals[6] = Integer.toString(da);
+			vals[7] = Integer.toString(v.getDate_from());
+			vals[8] = Integer.toString(v.getDate_to());
+			vals[9] = v.getVin();
+			vals[10] = v.getPlate();
+			vals[11] = Integer.toString(v.getOdometerOriginal());
+			vals[12] = Integer.toString(v.getOdometerCurrent());
+			vals[13] = v.getComment();
 		} else {
 			vals[1] = "Y";
 			vals[2] = Integer.toString(ownerIfNew.getID());
@@ -172,17 +180,17 @@ public abstract class MiscTablesCRUDDialogs
 				mainDriv = new Person(conn, Integer.parseInt(vals[2]));
 			} catch (Exception e) {}
 		}
-		final int date_from = (vals[6] != null) ? Integer.parseInt(vals[6]) : 0,
-		          date_to = (vals[7] != null) ? Integer.parseInt(vals[7]) : 0,
-		          odo_curr = (vals[11] != null) ? Integer.parseInt(vals[11]) : 0;
+		final int date_from = (vals[7] != null) ? Integer.parseInt(vals[7]) : 0,
+		          date_to   = (vals[8] != null) ? Integer.parseInt(vals[8]) : 0,
+		          odo_curr = (vals[12] != null) ? Integer.parseInt(vals[12]) : 0;
 		if (v == null)
 		{
 			if (mainDriv == null)
 				mainDriv = ownerIfNew;
-			final int odo_orig = (vals[10] != null) ? Integer.parseInt(vals[10]) : 0;
+			final int odo_orig = (vals[11] != null) ? Integer.parseInt(vals[11]) : 0;
 			v = new Vehicle
 			    (vals[0], mainDriv, Integer.parseInt(vals[3]), vals[4], Integer.parseInt(vals[5]),
-			     date_from, date_to, vals[8], vals[9], odo_orig, odo_curr, vals[12]);
+			     date_from, date_to, vals[9], vals[10], odo_orig, odo_curr, vals[13]);
 			v.setActive(isActive);
 			v.insert(conn);
 		} else if (mid.isChanged())
@@ -197,10 +205,10 @@ public abstract class MiscTablesCRUDDialogs
 			v.setYear((vals[5] != null) ? Integer.parseInt(vals[5]) : 0);
 			v.setDate_from(date_from);
 			v.setDate_to(date_to);
-			v.setVin(vals[8]);
-			v.setPlate(vals[9]);
+			v.setVin(vals[9]);
+			v.setPlate(vals[10]);
 			v.setOdometerCurrent(odo_curr);
-			v.setComment(vals[12]);
+			v.setComment(vals[13]);
 			v.commit();
 		}
 

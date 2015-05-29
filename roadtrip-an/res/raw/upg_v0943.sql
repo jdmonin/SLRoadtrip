65c60224bb87e20d7ce82d4c1e4085b7e4c21362
Jeremy D Monin <jdmonin@nand.net>
2015-05-28 21:57:49 -0500

    upg_v0943.sql: Correct (C) year, adjust whitespace

--- roadtrip-an/res/raw/upg_v0943.sql
+++ roadtrip-an/res/raw/upg_v0943.sql
@@ -1,19 +1,19 @@
 -- Upgrade from v0940 to v0943: (2015-05-26)
 
 -- This file is part of Shadowlands RoadTrip - A vehicle logbook for Android.
--- 
---  This file Copyright (C) 2014 Jeremy D Monin (jdmonin@nand.net)
--- 
+--
+--  This file Copyright (C) 2015 Jeremy D Monin (jdmonin@nand.net)
+--
 --  This program is free software: you can redistribute it and/or modify
 --  it under the terms of the GNU General Public License as published by
 --  the Free Software Foundation, either version 3 of the License, or
 --  (at your option) any later version.
--- 
+--
 --  This program is distributed in the hope that it will be useful,
 --  but WITHOUT ANY WARRANTY; without even the implied warranty of
 --  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 --  GNU General Public License for more details.
--- 
+--
 --  You should have received a copy of the GNU General Public License
 --  along with this program.  If not, see http://www.gnu.org/licenses/ .
 

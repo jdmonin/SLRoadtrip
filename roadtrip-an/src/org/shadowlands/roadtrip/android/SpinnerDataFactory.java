cf34b4ce109f6caae8310bd2e02501dedb736342
Jeremy D Monin <jdmonin@nand.net>
2017-01-16 07:35:17 -0500

    SpinnerDataFactory javadoc: Reminder to call i18n init localization

--- roadtrip-an/src/org/shadowlands/roadtrip/android/SpinnerDataFactory.java
+++ roadtrip-an/src/org/shadowlands/roadtrip/android/SpinnerDataFactory.java
@@ -1,7 +1,7 @@
 /*
  *  This file is part of Shadowlands RoadTrip - A vehicle logbook for Android.
  *
- *  This file Copyright (C) 2010-2015 Jeremy D Monin <jdmonin@nand.net>
+ *  This file Copyright (C) 2010-2015,2017 Jeremy D Monin <jdmonin@nand.net>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -22,6 +22,7 @@ package org.shadowlands.roadtrip.android;
 import org.shadowlands.roadtrip.db.GeoArea;
 import org.shadowlands.roadtrip.db.Person;
 import org.shadowlands.roadtrip.db.RDBAdapter;
+import org.shadowlands.roadtrip.db.RDBRecord;
 import org.shadowlands.roadtrip.db.TripCategory;
 import org.shadowlands.roadtrip.db.Vehicle;
 
@@ -37,6 +38,11 @@ import android.widget.Spinner;
  * such as {@link Vehicle} or {@link Person} (driver).
  * Also contains helper class {@link SpinnerItemSelectedListener}
  * for use in dialogs and activities.
+ *<P>
+ *<H5>I18N:<H5>
+ * Before using these call {@link RDBRecord#localizeStatics(String, String)}
+ * to localize some values, such as GeoArea "(none)" used by
+ * {@link #setupGeoAreasSpinner(RDBAdapter, Context, Spinner, int, boolean, int)}.
  *
  * @author jdmonin
  */
